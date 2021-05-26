47e168f2a115a3ae87d7a8c037b9a94139de931b
RyanMayWebdev <83085214+RyanMayWebdev@users.noreply.github.com>
2021-05-26 18:28:58 -0400

    initial upload

---
+++ JavaScript/index.js
@@ -0,0 +1,56 @@
+const navLinks = document.querySelectorAll(".navLink");
+let isMobile = false;
+
+if (window.screen.width <= 768) {
+    isMobile = true;
+}
+let lastScrollTop = window.pageYOffset;
+document.querySelector("#menuDrop").addEventListener("click", mobileMenuToggle);
+if (!isMobile) {
+    document.addEventListener("scroll", () => {
+        let currentYPos = window.pageYOffset;
+        const button = document.querySelector(".backToTopBtn");
+        if (currentYPos <= 5) {
+            button.classList.remove("revealElement");
+            button.classList.add("hideElement");
+        } else {
+            button.classList.add("revealElement");
+        }
+
+    });
+}
+for (let link of navLinks) {
+    link.addEventListener("click", mobileMenuToggle);
+}
+
+function mobileMenuToggle() {
+    document.querySelector(".mobileDropdown").classList.toggle("revealElement");
+}
+
+const observerOptions = {
+    root: document,
+    rootMargin: "0px",
+    threshold: 0.3
+};
+
+function observerCallback(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("fadeIn");
+        }
+    });
+}
+
+const fadeElements = document.querySelectorAll(".fade");
+const enlargeElements = document.querySelectorAll(".flexContainer div");
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+fadeElements.forEach(element => observer.observe(element));
+
+for (let index of enlargeElements){
+    index.addEventListener("mouseover",enlarge);
+    index.addEventListener("mouseout",enlarge);
+}
+function enlarge() {
+    this.classList.toggle("fadeEnlarge");
+}
